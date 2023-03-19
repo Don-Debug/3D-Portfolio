c49a516595e41c6b4ef7c9888bac0567e806aa59
debug <elision.q23@gmail.com>
2023-03-19 16:28:46 +0100

    no loading for now

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,11 @@
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+// import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
-const Loading = () => {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <ClimbingBoxLoader color="#f97316" size={30} />
-    </div>
-  );
-};
+// const Loading = () => {
+//   return (
+//     <div className="flex items-center justify-center h-screen">
+//       <ClimbingBoxLoader color="#f97316" size={30} />
+//     </div>
+//   );
+// };
 
-export default Loading;
+// export default Loading;
