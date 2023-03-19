f8ba6f276f95df4cd1b2c94b5b6c38f3a2d7348b
debug <elision.q23@gmail.com>
2023-03-19 16:56:05 +0100

    ok

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,21 +2,15 @@ import { TechBalls } from "./canvas/index";
 import { SectionFixer } from "../fixed/index";
 import { technologies } from "../constants/index";
 
-import { motion } from "framer-motion";
-import { zoomIn } from "../utils/motion";
-
 const Tech = () => {
   return (
-    <motion.div
-      variants={zoomIn()}
-      className=" flex flex-row flex-wrap justify-center gap-10"
-    >
+    <div className=" flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className=" w-28 h-28" key={technology.icon}>
           <TechBalls icon={technology.icon} />
         </div>
       ))}
-    </motion.div>
+    </div>
   );
 };
 
