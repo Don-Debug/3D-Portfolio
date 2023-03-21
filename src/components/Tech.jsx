b174856b1f6062a386dc2bd07e42c436fcf19b68
debug <elision.q23@gmail.com>
2023-03-21 21:26:39 +0100

    ok

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,23 +1,12 @@
-import { useState, useEffect } from "react";
 import { TechBalls } from "./canvas/index";
-import { technologies } from "../constants/index";
 import { SectionFixer } from "../fixed/index";
+import { technologies } from "../constants/index";
 
 const Tech = () => {
-  const [currentTech, setCurrentTech] = useState(0);
-
-  useEffect(() => {
-    const animationId = requestAnimationFrame(() => {
-      setCurrentTech((prev) => prev + 1);
-    });
-
-    return () => cancelAnimationFrame(animationId);
-  }, [currentTech]);
-
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.slice(0, currentTech + 1).map((technology) => (
-        <div className="w-28 h-28" key={technology.icon}>
+    <div className=" flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className=" w-28 h-28" key={technology.icon}>
           <TechBalls icon={technology.icon} />
         </div>
       ))}
