45c9c03b892358e5d9f651802054331b76234090
debug <elision.q23@gmail.com>
2023-03-21 20:54:41 +0100

    ok

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,16 +2,12 @@ import { TechBalls } from "./canvas/index";
 import { SectionFixer } from "../fixed/index";
 import { technologies } from "../constants/index";
 
-import LazyLoad from "react-lazyload";
-
 const Tech = () => {
   return (
     <div className=" flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className=" w-28 h-28" key={technology.icon}>
-          <LazyLoad offset={100}>
-            <TechBalls icon={technology.icon} />
-          </LazyLoad>
+          <TechBalls icon={technology.icon} />
         </div>
       ))}
     </div>
